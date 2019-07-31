24e444f0544b57b508ec875e310eb441f3c3feb5
veera <charlie84262@gmail.com>
2019-07-31 22:03:47 +0530

    Integrated with Firebase successfully

--- app/src/main/java/com/example/atlasmessager/RegistrationActivity.kt
+++ app/src/main/java/com/example/atlasmessager/RegistrationActivity.kt
@@ -15,6 +15,7 @@ class RegistrationActivity : Activity() {
         val userName = main_reg_username.text.toString()
         val email = main_reg_email.text.toString()
         val password = main_reg_password.text.toString()
+        
 
         main_reg_already_have_an_account.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java).apply{
